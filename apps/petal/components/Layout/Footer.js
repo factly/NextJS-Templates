026e052220faffddf4db6d5c5570428005079372
Chakradhar B <chakradhar2711@gmail.com>
2023-05-16 17:54:32 +0530

    change in navbar and footer for petal theme

--- apps/petal/components/Layout/Footer.js
+++ apps/petal/components/Layout/Footer.js
@@ -1,20 +1,34 @@
 /** @jsx jsx */
 /** @jsxRuntime classic */
+/** @jsx jsx */
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { jsx } from 'theme-ui';
+import Link from 'next/link';
+import { TbBrandFacebook } from 'react-icons/tb';
+import { TbBrandTwitter } from 'react-icons/tb'
+
 
 const Footer = () => (
-  <footer
-    sx={{
-      width: '100%',
-      mt: '2rem',
-      mb: '1rem',
-    }}
-  >
-    <p sx={{ textAlign: 'center' }}>
-      © 2022 Petal - Created and maintained by Factly Media and Research
+  <footer sx={{
+    display: 'flex',
+    flexDirection: 'column',
+    mt: '2rem',
+    padding: '24px',
+    alignItems: 'center',
+    background: '#5F3CC0',
+    color: '#D6BBFB',
+  }}>
+    <h2 sx={{ mt: '2rem', fontSize: '32px' }}>Kite</h2>
+    <p sx={{ mt: '1rem' }}>Kite is a modern, clean and dashboard style Premium theme. Build a community around your content or create your own platform.</p>
+    <div sx={{ display: 'flex', fontSize: '24px', gap: '1rem', mt: '1rem', a: { bg: '#f5f5f5', borderRadius: '50%', padding: '8px' } }}>
+      <Link href=''><TbBrandFacebook /></Link>
+      <Link href=''><TbBrandTwitter /></Link>
+    </div>
+    <p sx={{ fontSize: '0.875rem', mt: '1rem', mb: "2rem" }}>
+      © {new Date().getFullYear()} Kite - Created and maintained by Factly Media and Research
     </p>
   </footer>
 );
 
 export default Footer;
+
