e5509eaebd55b4c1b9fa6b9b6bd9023a4b1e9279
shreeharsha-factly <harsha@factly.in>
2024-12-26 21:32:13 +0530

    web: update client config

--- apps/iris/store/client.js
+++ apps/iris/store/client.js
@@ -1,16 +1,11 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import 'isomorphic-unfetch';
-import getConfig from 'next/config';
-
-const { publicRuntimeConfig } = getConfig();
 
 export const client = new ApolloClient({
-  uri: 'https://stag-dega-api.factly.in/query',
+  uri: process.env.NX_PUBLIC_DEGA_URL,
   headers: {
     'X-Dega-API-Key': process.env.NX_PUBLIC_DEGA_API_KEY,
-    'X-Space': parseInt(process.env.NX_PUBLIC_SPACE_ID)
+    'X-Space': parseInt(process.env.NX_PUBLIC_DEGA_SPACE_ID, 10),
   },
   cache: new InMemoryCache(),
 });
-
-
